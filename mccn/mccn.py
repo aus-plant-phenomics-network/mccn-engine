19d4ed88656036d4a63628f6e12646a0459a79ae
hoangson <lehoangsonsg@gmail.com>
2024-10-21 15:41:54 +1030

    chore: lint

--- mccn/mccn.py
+++ mccn/mccn.py
@@ -1,14 +1,14 @@
-from concurrent.futures import ThreadPoolExecutor
 import multiprocessing
+from collections.abc import Iterator
+from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
-from typing import Iterator, List, Optional, Dict
 
-from odc.geo.geobox import GeoBox
-from odc.stac import stac_load
 import pystac_client
-from tqdm import tqdm
 import rioxarray
 import xarray
+from odc.geo.geobox import GeoBox
+from odc.stac import stac_load
+from tqdm import tqdm
 from xarray import DataArray, Dataset
 
 from mccn.loader import stac_load_vector  # , ShapefileDriver
@@ -28,42 +28,55 @@ class Mccn:
         query = self.stac_client.search(collections=[col_id])
         return query.items()
 
-    def _load_stac(self, col_id: str, bands: Optional[List[str]] = None, groupby: str = "id",
-                  crs: Optional[str] = None, geobox: Optional[GeoBox] = None,
-                  lazy: bool = False) -> xarray.Dataset:
+    def _load_stac(
+        self,
+        col_id: str,
+        bands: Optional[List[str]] = None,
+        groupby: str = "id",
+        crs: Optional[str] = None,
+        geobox: Optional[GeoBox] = None,
+        lazy: bool = False,
+    ) -> xarray.Dataset:
         # TODO: Expose other parameters to the stac_load function
         print(f"Loading data for {col_id}.")
         pool = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
         # To lazy load we need to pass something to the chunks parameter of stac_load() function.
         if lazy:
-            chunks = {'x': 2048, 'y': 2048}
+            chunks = {"x": 2048, "y": 2048}
         else:
             chunks = None
         # TODO: We likely need a unique load function here depending on data type. For example
         # TODO: we need to write at least one for raster data and time series data.
 
         xx = stac_load(
-            self._query(col_id), bands=bands, groupby=groupby, chunks=chunks,  # type: ignore
-            progress=tqdm, pool=pool, crs=crs, geobox=geobox
+            self._query(col_id),
+            bands=bands,
+            groupby=groupby,
+            chunks=chunks,  # type: ignore
+            progress=tqdm,
+            pool=pool,
+            crs=crs,
+            geobox=geobox,
         )
-        if 'x' in xx.xindexes and 'y' in xx.xindexes:
+        if "x" in xx.xindexes and "y" in xx.xindexes:
             min_lon = xx.x.min().item()
             max_lon = xx.x.max().item()
             min_lat = xx.y.min().item()
             max_lat = xx.y.max().item()
-        elif 'longitude' in xx.xindexes and 'latitude' in xx.xindexes:
+        elif "longitude" in xx.xindexes and "latitude" in xx.xindexes:
             min_lon = xx.longitude.min().item()
             max_lon = xx.longitude.max().item()
             min_lat = xx.latitude.min().item()
             max_lat = xx.latitude.max().item()
         else:
-            raise AttributeError("Spatial axes must contain either x and y or "
-                                 "longitude and latitude.")
+            raise AttributeError(
+                "Spatial axes must contain either x and y or " "longitude and latitude."
+            )
         self.bbox = [min_lon, min_lat, max_lon, max_lat]
         return xx
 
     @staticmethod
-    def load_public(source: str, bbox: List[float], layername=None):
+    def load_public(source: str, bbox: list[float], layername=None):
         # Demo basic load function for WCS endpoint. Only DEM is currently supported.
         response = WcsImporterFactory().get_wcs_importer(source).get_data(bbox, layername)
         return rioxarray.open_rasterio(BytesIO(response.read()))
@@ -81,9 +94,17 @@ class Mccn:
             vmax=int(xx_0.max()),
         )
 
-    def load(self, col_id: str, bands: Optional[List[str]] = None, groupby: str = "id",
-             crs: Optional[str] = None, geobox: Optional[GeoBox] = None, lazy: bool = False,
-             source: Optional[Dict[str, str]] = None, mask=None):
+    def load(
+        self,
+        col_id: str,
+        bands: Optional[List[str]] = None,
+        groupby: str = "id",
+        crs: Optional[str] = None,
+        geobox: Optional[GeoBox] = None,
+        lazy: bool = False,
+        source: Optional[Dict[str, str]] = None,
+        mask=None,
+    ):
         """
         Load the STAC items for a given collection ID into an xarray dataset. Several options are
         available for sub-selection and transformation of the data upon loading.
@@ -98,12 +119,11 @@ class Mccn:
         :param mask:
         :return:
         """
-        xx: Dataset | DataArray = self._load_stac(col_id, bands=bands, groupby=groupby, crs=crs,
-                                                  geobox=geobox, lazy=lazy)
+        xx: Dataset | DataArray = self._load_stac(
+            col_id, bands=bands, groupby=groupby, crs=crs, geobox=geobox, lazy=lazy
+        )
         if mask and geobox:
-            vector_array = stac_load_vector(
-                list(self._query(col_id)), gbox=geobox
-            )
+            vector_array = stac_load_vector(list(self._query(col_id)), gbox=geobox)
             # TODO: Combine rasterised vector layers with other raster layers. Below is placeholder
             # TODO: to appease ruff.
             xx = xarray.combine_by_coords([xx, vector_array])
@@ -112,15 +132,16 @@ class Mccn:
         if source is not None and self.bbox:
             for source_name, layer_name in source.items():
                 if source_name != "dem":
-                    raise NotImplementedError(f"Datacube stacking for {source_name} has not been"
-                                              f"implemented.")
+                    raise NotImplementedError(
+                        f"Datacube stacking for {source_name} has not been" f"implemented."
+                    )
                 yy = self.load_public(source=source_name, bbox=self.bbox, layername=layer_name)
                 yy = yy.rename({"x": "longitude", "y": "latitude"})
                 yy = yy.interp(
                     longitude=list(xx.longitude.values),
                     latitude=list(xx.latitude.values),
                     method="linear",
-                    kwargs={"fill_value": "extrapolate"}
+                    kwargs={"fill_value": "extrapolate"},
                 )
                 # While the datasets have the same EPSG, they are defined differently in the
                 # spatial_ref layer. This aligns them but have to investigate further.
