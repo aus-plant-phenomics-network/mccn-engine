882f2b7a27d79a5864bce2cb6eea90fcd88b7d46
harryle <lehoangsonsg@gmail.com>
2024-11-06 09:57:39 +1030

    MCCN-130: add features for point data loader

--- mccn/pipeline.py
+++ mccn/pipeline.py
@@ -1,6 +1,6 @@
 import abc
 from collections.abc import Iterable, Sequence
-from typing import Generic, TypeVar, cast
+from typing import Generic, Literal, TypeVar, cast
 
 import geopandas as gpd
 import pandas as pd
@@ -17,6 +17,10 @@ class StacExtensionError(Exception): ...
 
 
 T = TypeVar("T")
+SupportedInterp = (
+    Literal["linear", "nearest", "zero", "slinear", "quadratic", "cubic", "polynomial"]
+    | Literal["barycentric", "krogh", "pchip", "spline", "akima", "makima"]
+)
 
 
 class Pipeline(abc.ABC, Generic[T]):
@@ -24,7 +28,7 @@ class Pipeline(abc.ABC, Generic[T]):
         self,
         items: Iterable[pystac.Item],
         geobox: GeoBox,
-        interp: str | None = None,
+        interp: SupportedInterp = "nearest",
         fields: Sequence[str] | dict[str, Sequence[str]] | None = None,
     ) -> None:
         self._items = items
@@ -47,36 +51,46 @@ class Pipeline(abc.ABC, Generic[T]):
         ds = self.to_xarray(items)
         return self.interp(ds, self._geobox, self._interp)
 
-    @abc.abstractmethod
     @staticmethod
+    @abc.abstractmethod
     def load_item(
         item: pystac.Item,
         fields: Sequence[str] | None = None,
     ) -> T:
+        """Load one single item. Output item is of the type parameterised by the subclass."""
         raise NotImplementedError
 
-    @abc.abstractmethod
     @staticmethod
+    @abc.abstractmethod
     def load_items(
         item: Iterable[pystac.Item],
         fields: Sequence[str] | dict[str, Sequence[str]] | None = None,
     ) -> T:
+        """Load all items. Individual items are obtained by invoking load_item"""
         raise NotImplementedError
 
-    @abc.abstractmethod
     @staticmethod
+    @abc.abstractmethod
     def query_bbox(src: T, crs: CRS | None = None, bbox: BBox_T | None = None) -> T:
+        """Restrict data to region specified by bbox"""
         raise NotImplementedError
 
-    @abc.abstractmethod
     @staticmethod
+    @abc.abstractmethod
     def to_xarray(src: T) -> xr.Dataset:
+        """Convert parameterised type to xarray dataset for downstream processing"""
         raise NotImplementedError
 
-    @abc.abstractmethod
     @staticmethod
-    def interp(src: xr.Dataset, geobox: GeoBox, method: str | None) -> xr.Dataset:
-        raise NotImplementedError
+    def interp(
+        src: xr.Dataset,
+        geobox: GeoBox,
+        method: SupportedInterp,
+    ) -> xr.Dataset:
+        """Interpolate an xarray dataset to a grid specified by geobox"""
+        X = [(geobox.affine * (i, 0))[0] for i in range(geobox.shape.x)]
+        Y = [(geobox.affine * (0, i))[1] for i in range(geobox.shape.y)]
+        return src.interp(X=X, Y=Y, method=method)
 
 
 class CsvPipeline(Pipeline[gpd.GeoDataFrame]):
@@ -148,8 +162,9 @@ class CsvPipeline(Pipeline[gpd.GeoDataFrame]):
 
     @staticmethod
     def to_xarray(src: gpd.GeoDataFrame) -> xr.Dataset:
+        # X and Y are default X name and Y name for merging dim
         src["X"] = src["geometry"].apply(lambda item: item.x)
         src["Y"] = src["geometry"].apply(lambda item: item.y)
-        src.drop(columns="geometry")
+        src.drop(columns="geometry", inplace=True)
         src.set_index(["X", "Y"], inplace=True, drop=True)
         return src.to_xarray()  # type: ignore[no-any-return]
